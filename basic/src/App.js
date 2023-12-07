36249db745f8bb7690d460872a3e0e8ea0213518
W <85074303+WencySuo@users.noreply.github.com>
2023-12-07 01:25:03 +0000

    stuff

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,11 +2,18 @@
 import React from 'react';
 import Map from './Map';
 import Nav from './Nav';
+import PostForm from './PostForm';
 
 function App() {
+  const handlePostSubmit = (message) => {
+    // add logic to handle the post submission 
+    console.log('Post submitted:', message);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Nav />
+      <PostForm onPostSubmit={handlePostSubmit} />
       <Map />
     </div>
   );
--- basic/src/Nav.js
+++ basic/src/Nav.js
@@ -2,7 +2,11 @@
 import React from 'react';
 import logo from './assets/remylogo_tpt.png';
 
-const Nav = () => {
+const Nav = ({ onPostSubmit }) => {
+    const handlePostButtonClick = () => {
+        // trigger the post submission logic from the parent component 
+        onPostSubmit('Message from Navbar Button');
+    };
     return (
         // navbar code 
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
@@ -12,7 +16,13 @@ const Nav = () => {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RemyGram</span>
                 </div>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" className="text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-500 dark:focus:ring-orange-600">Sign In</button>
+                    <button 
+                        type="button" 
+                        className="text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-500 dark:focus:ring-orange-600"
+                        onClick={handlePostButtonClick}
+                    >
+                        Add Post
+                    </button>
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
