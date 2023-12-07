8ebd7145c26863487c4c6d1e2ffb0083aad2600e
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 04:05:23 +0000

    mysql

--- basic/src/flask/app.py
+++ basic/src/flask/app.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session, send_from_directory
-from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 
 # Configure application
@@ -11,21 +10,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Youarenothing7!@lo
 
 # Setting up databases
 db = SQLAlchemy(app)
-db.reflect()
-# Access the existing tables
-users = db.Model.metadata.tables['users']
-posts = db.Model.metadata.tables['posts']
-comments = db.Model.metadata.tables['comments']
-likes = db.Model.metadata.tables['likes']
-class Users(db.Model):
-    __table__ = users
-class Posts(db.Model):
-    __table__ = posts
-class Comments(db.Model):
-    __table__ = comments
-class Likes(db.Model):
-    __table__ = likes
+with app.app_context():
+    db.reflect()
+
+    # Access the existing tables after reflection
+    users_table = db.Model.metadata.tables['users']
+    posts_table = db.Model.metadata.tables['posts']
+    comments_table = db.Model.metadata.tables['comments']
+    likes_table = db.Model.metadata.tables['likes']
+
+    # Define models based on reflected tables
+    class Users(db.Model):
+        __table__ = users_table
+
+    class Posts(db.Model):
+        __table__ = posts_table
+
+    class Comments(db.Model):
+        __table__ = comments_table
+
+    class Likes(db.Model):
+        __table__ = likes_table
 
 @app.route('/')
 def index():
-    
+    # Query users from the database
+    users = Users.query.all()
+    return render_template('test.html', users=users)
