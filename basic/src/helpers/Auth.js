3159faa816613f11ad9fc9e315627475998d8107
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 14:56:06 +0000

    auth helper

--- basic/src/helpers/Auth.js
+++ basic/src/helpers/Auth.js
@@ -2,30 +2,19 @@
 import { auth, googleProvider } from "../config/firebase";
 import { signInWithPopup, signOut } from "firebase/auth";
 
-export const Auth = () => {
-    console.log(auth?.currentUser?.email, auth?.currentUser?.displayName)
-
-    // Uses Firebase's built-in Google authentication service to log the user in and out
-    const signIn = async () => {
-        try {
-            await signInWithPopup(auth, googleProvider);
-        } catch (err) {
-            console.log(err);
-        }
+// Uses Firebase's built-in Google authentication service to log the user in and out
+export const signIn = async () => {
+    try {
+        await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+        console.log(err);
     }
+}
 
-    const signOut = async () => {
-        try {
-            await signOut(auth);
-        } catch (err) {
-            console.log(err);
-        }
+export const signOut = async () => {
+    try {
+        await signOut(auth);
+    } catch (err) {
+        console.log(err);
     }
-
-    return (
-        <div>
-            <button onClick={signIn}>Sign In</button>
-            <button onClick={signOut}>Sign Out</button>
-        </div>
-    )
 }
