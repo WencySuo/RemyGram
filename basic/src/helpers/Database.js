4b4b65fc1364a8d6ed1baca181e4d8f42ce3d2b1
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 12:44:03 -0500

    export db

---
+++ basic/src/helpers/Database.js
@@ -0,0 +1,10 @@
+// Hopefully every request to the database can be routed through here.
+// TODO: Import database stuff
+
+// TODO: addUser
+
+// TODO: addPost
+
+// TODO: addComment
+
+// TODO: addLike
