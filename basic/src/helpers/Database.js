1d6e2c6f17deb63162c604025e41cc84d3da214b
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 13:50:34 -0500

    lklklk

--- basic/src/helpers/Database.js
+++ basic/src/helpers/Database.js
@@ -1,7 +1,30 @@
 // Hopefully every request to the database can be routed through here.
-// TODO: Import database stuff
+import { db } from "../config/firebase";
 
 // TODO: addUser
+export const addUser = async (email, username) => {
+    try {
+      // Generate a unique user ID
+      const userId = database().ref().child('users').push().key;
+  
+      // Construct the user object
+      const user = {
+        email: email,
+        username: username,
+        posts: {},
+        likes: {},
+        comments: {}, 
+      };
+  
+      // Save the user to the database under "users" collection with the generated ID
+      await database().ref(`users/${userId}`).set(user);
+  
+      return userId; // Return the generated user ID if needed
+    } catch (error) {
+      console.error('Error adding user:', error);
+      throw error; // Rethrow the error for handling in the UI
+    }
+  };
 
 // TODO: addPost
 
