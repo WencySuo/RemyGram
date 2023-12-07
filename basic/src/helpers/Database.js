68e07aa651e23dab1c6cf0c6021717451345fefc
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 14:53:19 -0500

    working on addpost

--- basic/src/helpers/Database.js
+++ basic/src/helpers/Database.js
@@ -1,5 +1,7 @@
 // Hopefully every request to the database can be routed through here.
 import { db, auth } from "../config/firebase";
+import { serverTimestamp } from "firebase/database";
+import { getFirestore, doc, setDoc, geoPoint } from "firebase/firestore";
 
 // If the user is logged in, creates a new entry in the database for them. Returns 1 if successful and 0 if not.
 export const addCurrentUser = () => {
@@ -47,8 +49,47 @@ export const isUserInDatabase = () => {
 }
 
 // TODO: addPost
+export const addPost = async (caption, imagePath, geo, sightTime) => {
+    try {
+        // Getting everything ready
+        uid = auth().currentUser.uid;
+        location = new firebase.firestore.GeoPoint(geo["geometry"]["coordinates"][1], geo["geometry"]["coordinates"][0]);
+
+        const postData = {
+            author: uid,
+            caption: caption,
+            comments: {},
+            imagePath: imagePath,
+            likes: {},
+            location: location,
+            metadata: {
+                numComments: 0,
+                numLikes: 0,
+                postTime: serverTimestamp()},
+            sightTime: sightTime,
+          };
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+// const addPost = async (author, caption, imagePath, geoJson, metadata) => {
+//   try {
+//     // Add the post to the database
+//     const newPostRef = push(ref(db, 'posts'));
+//     const postId = newPostRef.key;
+//     await setDoc(doc(firestore, 'posts', postId), postData);
+
+//     console.log("Post added successfully!");
+//   } catch (error) {
+//     console.error("Error adding post:", error);
+//   }
+// };
 
 
 // TODO: addComment
 
+
 // TODO: addLike
+
