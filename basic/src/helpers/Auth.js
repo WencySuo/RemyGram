3dc1bc0144ce61230aaf14a4ed05fd70d853db6c
Bvarhhsdb <144722911+Bvarhhsdb@users.noreply.github.com>
2023-12-07 12:34:24 -0500

    deleted redundant function

--- basic/src/helpers/Auth.js
+++ basic/src/helpers/Auth.js
@@ -3,13 +3,6 @@ import { auth, googleProvider } from "../config/firebase";
 import { signInWithPopup, signOut as firebaseSignOut } from "firebase/auth";
 
 // Uses Firebase's built-in Google authentication service to log the user in and out
-export const signIn = async () => {
-    try {
-        await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-        console.error(err);
-    }
-}
 
 export const signOut = async () => {
     try {
