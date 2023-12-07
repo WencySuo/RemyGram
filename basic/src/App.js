10c68ddfdea1c3d5f1dc5dc12c5702ce2beb1761
W <85074303+WencySuo@users.noreply.github.com>
2023-12-07 01:34:04 +0000

    post button

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,20 +1,23 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import Map from './Map';
 import Nav from './Nav';
 import PostForm from './PostForm';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
   const handlePostSubmit = (message) => {
-    // add logic to handle the post submission 
-    console.log('Post submitted:', message);
+    // Add logic to send the post to your database or API
+    // For now, let's just update the state to simulate a new post
+    setPosts((prevPosts) => [...prevPosts, { id: Date.now(), message }]);
   };
 
   return (
     <div className="flex flex-col h-screen">
       <Nav />
       <PostForm onPostSubmit={handlePostSubmit} />
-      <Map />
+      <Map posts={posts} />
     </div>
   );
 }
